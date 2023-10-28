69ad93cc971222f1941cb4a9d203927633e1394c
vladihka <vladihka58@gmail.com>
2023-10-28 13:17:45 +0200

    Install tailwind css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
     return (
-        <h1>HOME</h1>
-
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     )
 }
