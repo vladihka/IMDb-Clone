b860fec7c1a974e3fac102f4655932718d2f54a5
vladihka <vladihka58@gmail.com>
2023-10-29 15:02:17 +0100

    Fix

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,23 +1,19 @@
 'use client'
 import {GrFormNextLink, GrFormPreviousLink} from "react-icons/gr";
-import {useState} from "react";
 import {useRouter} from "next/navigation";
 
 export default function Test({params}){
 
-    //const [count, setCount] = useState(params?.id);
     let count = params?.id;
     const router = useRouter();
 
     function handleClickCountA(){
         count--;
-        //setCount(count-1);
         router.push(`/catalog/${count}`);
     }
 
     function handleClickCountB(){
         count++;
-        //setCount(count+1);
         router.push(`/catalog/${count}`);
     }
 
